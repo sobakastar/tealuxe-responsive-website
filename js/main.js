f47507395907560cced33dba6c7725a5fc22f7a0
sobakastar <dr.gris@yandex.ru>
2024-01-14 20:07:55 +0300

    init project

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menuClose = document.querySelector('.menu__close');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menuList.classList.toggle('menu__list--open')
+  });
+
+  menuClose.addEventListener('click', () => {
+    menuList.classList.remove('menu__list--open')
+    });
+
+
+
+
+
+
+AOS.init({
+  disable:'mobile'
+});
